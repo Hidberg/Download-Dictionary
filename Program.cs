8090f7a4b8f4716fc3d29e1dc4eb7eefac54ae83
Vyacheslav Hidberg <hidberg@yandex.ru>
2016-05-09 17:46:25 +0300

    Working version with many threads. Dont have pause, saved currentAdress, log file. All messages Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -7,69 +7,111 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using System.IO;
 using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace JSON_Deserialize
 {
     class Program
     {
+        static int countQuery = 254200; // 10 000 000 запросов это 1млрд строк. 254200 запросов это Сереги для проверки(25,42 млн)
+        static ConcurrentDictionary<int, string> data = new ConcurrentDictionary<int, string>();
+        static int queryOnThread;
+        static int currentQuery = 0;
+
         static void Main(string[] args)
         {
-            int count = 0;
-            int ticket = 0;
             string txtFromFile = "";
             try
             {
-                StreamReader config = new StreamReader("json_config.txt");
-                txtFromFile = config.ReadLine();
+                using (StreamReader config = new StreamReader("json_config.txt"))
+                {
+                    txtFromFile = config.ReadLine();
+                }
             }
             catch (Exception)
             {
                 Console.WriteLine("File doesnt exist. Need to be with exe file.");
                 Environment.Exit(1);
             }
-            string[] values = txtFromFile.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-            int i = Convert.ToInt32(values[0]);
-            int condition = Convert.ToInt32(values[1]);
-            StreamWriter file = new StreamWriter("json_result.txt", true);
+
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            while (count < condition)
+
+            int maxThreadsAllowed = Convert.ToInt32(txtFromFile);
+            Thread[] Threads = new Thread[maxThreadsAllowed];
+            queryOnThread = countQuery / maxThreadsAllowed;
+
+            for (int i = 0; i < maxThreadsAllowed; ++i)
             {
-                System.Uri uri = new Uri(string.Format("https://hola.org/challenges/word_classifier/testcase/{0}", i++));
-                HttpClient client = new HttpClient();
-                client.BaseAddress = uri;
-                string response;
-                response = client.GetStringAsync(uri).Result;
-                if (!string.IsNullOrEmpty(response))
-                {
-                    while(response == "Rate limit exceeded")
-                    {
-                        ++ticket;
-                        System.Threading.Thread.Sleep(1000);
-                        response = client.GetStringAsync(uri).Result;
-                    }
-                    Dictionary<string, bool> words = JsonConvert.DeserializeObject<Dictionary<string, bool>>(response);
-                    count += words.Count;
-                    foreach (var item in words)
-                    {
-                        if (!string.IsNullOrEmpty(response))
-                            file.WriteLine(string.Format(("{0} {1}"), item.Key, Convert.ToInt32(item.Value)));
-                    }
-                    if(count == 25420000)
-                    {
-                        ;
-                    }
-                }
-                else
-                {
-                    ++ticket;
-                }
-                client.Dispose();
+                Threads[i] = new Thread(GetDataFromServer);
+                Threads[i].Start();
             }
-            Console.WriteLine(ticket);
+            using (StreamWriter file = new StreamWriter("result.txt", true))
+            {
+                file.NewLine = "\n";
+                Console.WriteLine("Программа запущена...");
+                for (int i = 1; i <= countQuery;)
+                {
+                    string content;
+
+                    if (data.TryRemove(i, out content))
+                    {
+                        StringBuilder mutableContent = new StringBuilder(content);
+                        mutableContent = mutableContent.Replace("\"", "").Replace("{\n ", "").Replace("}", "").Replace(" ", "").Replace(",", "").Replace(":true", " 1").Replace(":false", " 0");
+                        file.Write(mutableContent.ToString());
+                        file.Flush();
+                        if (i % 10 == 0)
+                        {
+                            Console.WriteLine("Выполнено '{0}' запросов, записано '{1}' строк", i, i * 100);
+                        }
+                        ++i;
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(100); 
+                    }
+                }
+            }
             stopWatch.Stop();
-            Console.WriteLine(((stopWatch.ElapsedMilliseconds) / (1000)).ToString());
-            file.Dispose();
+            Console.WriteLine(((stopWatch.ElapsedMilliseconds) / 1000 ).ToString());
+        }
+
+        static void GetDataFromServer()
+        {
+            while (currentQuery < countQuery)
+            {
+                try
+                {
+                    int localCurrentQuery = Interlocked.Increment(ref currentQuery); // здесь ошибка
+                    HttpWebRequest http = WebRequest.CreateHttp("https://hola.org/challenges/word_classifier/testcase/" + localCurrentQuery);
+                    using (HttpWebResponse response = (HttpWebResponse)http.GetResponse())
+                    {
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                            {
+                                string content = sr.ReadToEnd();
+                                while (!data.TryAdd(localCurrentQuery, content))
+                                {
+                                    Console.WriteLine("Shit happens! TryAdd return false.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Запрос '{0}' вернул код '{1}'", localCurrentQuery, response.StatusCode);
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Error when getting data from client");
+                    Console.WriteLine(e.ToString());
+                    System.Threading.Thread.Sleep(10000);
+                }
+            }
         }
     }
 }
